81b6c749213197a09927aef6bfe0dd6b6b7eda51
Glen Park <glenpark@Glens-MacBook-Pro.local>
2023-03-09 11:29:05 -0800

    Adjustment to account for bug in TABLE_STORAGE view where deleted tables still show up.  Joining to the TABLES view resolves the issue

--- information_schema/billing_model_cost_estimation.sql
+++ information_schema/billing_model_cost_estimation.sql
@@ -1,38 +1,44 @@
 /*
 Query that calculates both logical and physical storage billing costs
-along with compression ratio (>2.0 means cost savings in physical 
-billing model).  Does not take into account 10gb free storage per month 
-given that is weighted equally between logical/physical so it cancels 
-out when it comes to considering which provides the most savings.  
+along with compression ratio (>2.0 means cost savings in physical
+billing model).  Does not take into account 10gb free storage per month
+given that is weighted equally between logical/physical so it cancels
+out when it comes to considering which provides the most savings.
 
 All numbers are grouped by dataset and considered estimates only.
 */
 DECLARE active_logical_gb_price FLOAT64 DEFAULT 0.02;
 DECLARE long_term_logical_gb_price FLOAT64 DEFAULT 0.01;
 DECLARE active_physical_gb_price FLOAT64 DEFAULT 0.04;
-DECLARE long_term_physical_gb_price FLOAT64 DEFAULT 0.02; 
+DECLARE long_term_physical_gb_price FLOAT64 DEFAULT 0.02;
 
-with storage_sizes as 
+with storage_sizes as
+(
+  select
+    a.project_id
+    ,a.table_schema
+    ,SUM(a.active_logical_bytes) / power(1000, 3) AS active_logical_gb
+    ,SUM(a.long_term_logical_bytes) / power(1000, 3) AS long_term_logical_gb
+    ,SUM(a.active_physical_bytes) / power(1000, 3) AS active_physical_gb
+    ,SUM(a.long_term_physical_bytes) / power(1000, 3) AS long_term_physical_gb
+    ,SUM(a.total_physical_bytes) / power(1000, 3) AS total_physical_gb
+    ,SUM(a.total_logical_bytes) / power(1000, 3) AS total_logical_gb
+  from
+  `<project-name>`.`<dataset-region>`.INFORMATION_SCHEMA.TABLE_STORAGE_BY_PROJECT a
+  join `<project-name>`.`<dataset-region>`.INFORMATION_SCHEMA.TABLES b
+  on a.project_id = b.table_catalog
+  and a.table_schema = b.table_schema
+  and a.table_name = b.table_name
+  WHERE a.total_logical_bytes > 0
+  AND a.total_physical_bytes > 0
+  GROUP BY a.project_id, a.table_schema
+)
+,cost_calcs as
 (
   select
     project_id
     ,table_schema
-    ,SUM(active_logical_bytes) / power(1000, 3) AS active_logical_gb
-    ,SUM(long_term_logical_bytes) / power(1000, 3) AS long_term_logical_gb
-    ,SUM(active_physical_bytes) / power(1000, 3) AS active_physical_gb
-    ,SUM(long_term_physical_bytes) / power(1000, 3) AS long_term_physical_gb
-    ,SUM(total_physical_bytes) / power(1000, 3) AS total_physical_gb
-    ,SUM(total_logical_bytes) / power(1000, 3) AS total_logical_gb
-  from
-  `<project-name>`.`<dataset-region>`.INFORMATION_SCHEMA.TABLE_STORAGE_BY_PROJECT
-  WHERE total_logical_bytes > 0
-  AND total_physical_bytes > 0
-  GROUP BY project_id,table_schema
-)
-,cost_calcs as
-(
-  select 
-    active_logical_gb
+    ,active_logical_gb
     ,active_logical_gb*active_logical_gb_price as active_logical_cost
     ,long_term_logical_gb
     ,long_term_logical_gb*long_term_logical_gb_price as long_term_logical_cost
@@ -44,7 +50,9 @@ with storage_sizes as
   from storage_sizes
 )
 select
-  active_logical_gb
+  project_id
+  ,table_schema
+  ,active_logical_gb
   ,long_term_logical_gb
   ,(active_logical_cost+long_term_logical_cost) as total_logical_cost
   ,active_physical_gb
